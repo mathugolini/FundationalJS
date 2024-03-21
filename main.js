ec6ed1f10d3fdd7327526a6c76afddf80294cb6d
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-03-21 16:44:29 -0300

    Math examples

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-let dataAtual = new Date();
+console.log(Math.PI); // Retorna o valor de pi (3.141592653589793)
+console.log(Math.E); // Retorna o valor da base do logaritmo natural (2.718281828459045)
 
-// Obter o dia da semana 
-//(0 para Domingo, 1 para Segunda-feira, ..., 6 para Sábado)
-let diaDaSemana = dataAtual.getDay();
-console.log(diaDaSemana);
+let numero = 4.6;
+
+console.log(Math.round(numero)); // Arredonda para o número inteiro mais próximo (5)
+console.log(Math.floor(numero)); // Arredonda para baixo para o número inteiro mais próximo (4)
+console.log(Math.ceil(numero)); // Arredonda para cima para o número inteiro mais próximo (5)
+
+console.log(Math.sqrt(numero)); // Calcula a raiz quadrada (2.1447610589527217)
+console.log(Math.random()); // Gera um número aleatório entre 0 (inclusivo) e 1 (exclusivo)
 
 
-// Convertendo uma data UTC para o horário local
-let dataUTCString = '2024-03-21T12:30:00Z';
-let dataLocalConvertida = new Date(dataUTCString);
-console.log(dataLocalConvertida);
+let graus = 45;
+
+let radianos = Math.PI * graus / 180; // Convertendo de graus para radianos
+console.log(radianos); // 0.7853981633974483
+
+console.log(Math.sin(radianos)); // Calcula o seno do ângulo (0.7071067811865475)
+console.log(Math.cos(radianos)); // Calcula o cosseno do ângulo (0.7071067811865476)
+console.log(Math.tan(radianos)); // Calcula a tangente do ângulo (1)
+
 
