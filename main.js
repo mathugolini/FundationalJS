f59366f1dbba692023830ea14133a630cc32417d
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-04-01 15:02:58 -0300

    switch examples

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
-console.log(Math.PI); // Retorna o valor de pi (3.141592653589793)
-console.log(Math.E); // Retorna o valor da base do logaritmo natural (2.718281828459045)
+switch (expressao) {
+  case valor1:
+    // bloco de código a ser executado se expressao for igual a valor1
+    break;
+  case valor2:
+    // bloco de código a ser executado se expressao for igual a valor2
+    break;
+  // e assim por diante
+  default:
+    // bloco de código a ser executado se nenhum caso corresponder à expressao
+}
 
-let numero = 4.6;
+let diaDaSemana = 2;
+let nomeDia;
 
-console.log(Math.round(numero)); // Arredonda para o número inteiro mais próximo (5)
-console.log(Math.floor(numero)); // Arredonda para baixo para o número inteiro mais próximo (4)
-console.log(Math.ceil(numero)); // Arredonda para cima para o número inteiro mais próximo (5)
+switch (diaDaSemana) {
+  case 1:
+    nomeDia = "Domingo";
+    break;
+  case 2:
+    nomeDia = "Segunda-feira";
+    break;
+  case 3:
+    nomeDia = "Terça-feira";
+    break;
+  case 4:
+    nomeDia = "Quarta-feira";
+    break;
+  case 5:
+    nomeDia = "Quinta-feira";
+    break;
+  case 6: 
+    nomeDia = "Sexta-feira";
+    break;
+  case 7:
+    nomeDia = "Sabado";
+    break;
+  default:
+    nomeDia = "Dia inválido";
+}
 
-console.log(Math.sqrt(numero)); // Calcula a raiz quadrada (2.1447610589527217)
-console.log(Math.random()); // Gera um número aleatório entre 0 (inclusivo) e 1 (exclusivo)
+console.log("Hoje é " + nomeDia);
 
 
-let graus = 45;
 
-let radianos = Math.PI * graus / 180; // Convertendo de graus para radianos
-console.log(radianos); // 0.7853981633974483
 
-console.log(Math.sin(radianos)); // Calcula o seno do ângulo (0.7071067811865475)
-console.log(Math.cos(radianos)); // Calcula o cosseno do ângulo (0.7071067811865476)
-console.log(Math.tan(radianos)); // Calcula a tangente do ângulo (1)
 
 
