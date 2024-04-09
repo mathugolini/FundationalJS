00e9b27a866b8b32a2660ef214f861e416040ce1
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-04-09 15:04:37 -0300

    for examples

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-while (condição) {
-  // bloco do código que vai ser executado enquando a condição 
-  // for verdadeira 
+for (inicialização; condição; expressão final) {
+  // bloco de código a ser repetido
 }
 
-
-let contador = 1;
-
-while (contador <= 5) {
-  console.log(contador);
-  contador++;
+for (let i=0; i<=5; i++) {
+  console.log(i);
 }
+
+for (let i = 0; i <= 5; i++) {
+  if (i === 3) {
+    continue; // Pula a iteração quando i é igual a 3
+  }
+  console.log(i);
+  if (i === 4) {
+    break; // Termina o loop quando i é igual a 4
+  }
+}
+
+let matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+for (let i=0; i<matriz.length; i++) {
+  for (let j=0; i<matriz[i].length; j++) {
+    console.log(matriz[i][j]);
+  }
+}
+
 
 
 
