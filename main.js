70beb4fd505f47a8d355eb5b0b7b16283fc78b10
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-04-09 15:03:53 -0300

    while exemples

--- main.js
+++ main.js
@@ -1,47 +1,15 @@
-switch (expressao) {
-  case valor1:
-    // bloco de código a ser executado se expressao for igual a valor1
-    break;
-  case valor2:
-    // bloco de código a ser executado se expressao for igual a valor2
-    break;
-  // e assim por diante
-  default:
-    // bloco de código a ser executado se nenhum caso corresponder à expressao
+while (condição) {
+  // bloco do código que vai ser executado enquando a condição 
+  // for verdadeira 
 }
 
-let diaDaSemana = 2;
-let nomeDia;
 
-switch (diaDaSemana) {
-  case 1:
-    nomeDia = "Domingo";
-    break;
-  case 2:
-    nomeDia = "Segunda-feira";
-    break;
-  case 3:
-    nomeDia = "Terça-feira";
-    break;
-  case 4:
-    nomeDia = "Quarta-feira";
-    break;
-  case 5:
-    nomeDia = "Quinta-feira";
-    break;
-  case 6: 
-    nomeDia = "Sexta-feira";
-    break;
-  case 7:
-    nomeDia = "Sabado";
-    break;
-  default:
-    nomeDia = "Dia inválido";
+let contador = 1;
+
+while (contador <= 5) {
+  console.log(contador);
+  contador++;
 }
-
-console.log("Hoje é " + nomeDia);
-
-
 
 
 
